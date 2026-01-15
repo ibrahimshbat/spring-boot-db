d377edebe9fd748ec90abd150069c21269586d55
Ibrahim EL-Sanosi <ibrahimsabat@yahoo.com>
2026-01-15 22:25:36 +0000

    Set up GetHub Action

--- src/test/java/com/demo/db/demo/DbApplicationTests.java
+++ src/test/java/com/demo/db/demo/DbApplicationTests.java
@@ -2,12 +2,14 @@ package com.demo.db.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Use application-test.properties
 class DbApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test just ensures the Spring context loads successfully
 	}
-
 }
